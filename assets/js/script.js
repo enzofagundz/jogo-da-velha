7b4bd472767acf93fbb93428d7f7a46b1e46a637
Enzo Fagundes <enzo.fagundes014@gmail.com>
2022-10-01 09:47:21 -0300

    verificar vitoria e botao para reiniciar game adicionados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,47 @@
 const casa = Array.from(document.querySelectorAll('.casa'));
-let tabuleiro = ['', '', '', '', '', '', '' , '', ''];
+const players = [1, 2];
+
 let sessao = true;
 let jogada;
-const players = [1, 2];
+let reiniciar = document.querySelector('#btn-reiniciar');
+
 let sortear = Math.floor(Math.random() * players.length);
 
+
+reiniciar.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    location.reload();
+}); 
+
+function verificarVitoria () {
+    if (casa[0].innerHTML === '<div class="playerOne"></div>' && casa[1].innerHTML === '<div class="playerOne"></div>' && casa[2].innerHTML === '<div class="playerOne"></div>'
+    || casa[3].innerHTML === '<div class="playerOne"></div>' && casa[4].innerHTML === '<div class="playerOne"></div>' && casa[5].innerHTML === '<div class="playerOne"></div>'
+    || casa[6].innerHTML === '<div class="playerOne"></div>' && casa[7].innerHTML === '<div class="playerOne"></div>' && casa[8].innerHTML === '<div class="playerOne"></div>'
+    || casa[0].innerHTML === '<div class="playerOne"></div>' && casa[3].innerHTML === '<div class="playerOne"></div>' && casa[6].innerHTML === '<div class="playerOne"></div>'
+    || casa[1].innerHTML === '<div class="playerOne"></div>' && casa[4].innerHTML === '<div class="playerOne"></div>' && casa[7].innerHTML === '<div class="playerOne"></div>'
+    || casa[2].innerHTML === '<div class="playerOne"></div>' && casa[5].innerHTML === '<div class="playerOne"></div>' && casa[8].innerHTML === '<div class="playerOne"></div>'
+    || casa[0].innerHTML === '<div class="playerOne"></div>' && casa[4].innerHTML === '<div class="playerOne"></div>' && casa[8].innerHTML === '<div class="playerOne"></div>'
+    || casa[2].innerHTML === '<div class="playerOne"></div>' && casa[4].innerHTML === '<div class="playerOne"></div>' && casa[6].innerHTML === '<div class="playerOne"></div>'
+    )
+    {
+        setTimeout(() => {
+            alert('Bolinha ganhou');
+        }, 2000);
+    }
+    else if (casa[0].innerHTML === '<div class="playerTwo"></div>' && casa[1].innerHTML === '<div class="playerTwo"></div>' && casa[0].innerHTML === '<div class="playerTwo"></div>'
+    || casa[3].innerHTML === '<div class="playerTwo"></div>' && casa[4].innerHTML === '<div class="playerTwo"></div>' && casa[5].innerHTML === '<div class="playerTwo"></div>'
+    || casa[6].innerHTML === '<div class="playerTwo"></div>' && casa[7].innerHTML === '<div class="playerTwo"></div>' && casa[8].innerHTML === '<div class="playerTwo"></div>'
+    || casa[0].innerHTML === '<div class="playerTwo"></div>' && casa[3].innerHTML === '<div class="playerTwo"></div>' && casa[6].innerHTML === '<div class="playerTwo"></div>'
+    || casa[1].innerHTML === '<div class="playerTwo"></div>' && casa[4].innerHTML === '<div class="playerTwo"></div>' && casa[7].innerHTML === '<div class="playerTwo"></div>'
+    || casa[2].innerHTML === '<div class="playerTwo"></div>' && casa[5].innerHTML === '<div class="playerTwo"></div>' && casa[8].innerHTML === '<div class="playerTwo"></div>'
+    || casa[0].innerHTML === '<div class="playerTwo"></div>' && casa[4].innerHTML === '<div class="playerTwo"></div>' && casa[8].innerHTML === '<div class="playerTwo"></div>'
+    || casa[2].innerHTML === '<div class="playerTwo"></div>' && casa[4].innerHTML === '<div class="playerTwo"></div>' && casa[6].innerHTML === '<div class="playerTwo"></div>')
+    {
+        setTimeout(() => {
+            alert('Xis ganhou');
+        }, 2000);
+    }
+}
 
 casa[0].addEventListener ('click', function () {
 
@@ -29,6 +66,8 @@ casa[0].addEventListener ('click', function () {
 
         casa[0].appendChild(playerAtual);
     }
+
+    verificarVitoria();
 });
 
 casa[1].addEventListener ('click', function () {
@@ -54,6 +93,8 @@ casa[1].addEventListener ('click', function () {
 
         casa[1].appendChild(playerAtual);
     }
+
+    verificarVitoria();
 });
 
 casa[2].addEventListener ('click', function () {
@@ -79,6 +120,8 @@ casa[2].addEventListener ('click', function () {
 
         casa[2].appendChild(playerAtual);
     }
+
+    verificarVitoria();
 });
 
 casa[3].addEventListener ('click', function () {
@@ -104,6 +147,8 @@ casa[3].addEventListener ('click', function () {
 
         casa[3].appendChild(playerAtual);
     }
+
+    verificarVitoria();
 });
 
 casa[4].addEventListener ('click', function () {
@@ -129,6 +174,8 @@ casa[4].addEventListener ('click', function () {
 
         casa[4].appendChild(playerAtual);
     }
+
+    verificarVitoria();
 });
 
 casa[5].addEventListener ('click', function () {
@@ -154,11 +201,13 @@ casa[5].addEventListener ('click', function () {
 
         casa[5].appendChild(playerAtual);
     }
+
+    verificarVitoria();
 });
 
 casa[6].addEventListener ('click', function () {
 
-    if(casa[6].innerHTML === '<div class="playerOne"></div>' || casa[1].innerHTML === '<div class="playerTwo"></div>')
+    if(casa[6].innerHTML === '<div class="playerOne"></div>' || casa[6].innerHTML === '<div class="playerTwo"></div>')
     {
         jogada = false;
     }
@@ -179,11 +228,13 @@ casa[6].addEventListener ('click', function () {
 
         casa[6].appendChild(playerAtual);
     }
+
+    verificarVitoria();
 });
 
 casa[7].addEventListener ('click', function () {
 
-    if(casa[7].innerHTML === '<div class="playerOne"></div>' || casa[1].innerHTML === '<div class="playerTwo"></div>')
+    if(casa[7].innerHTML === '<div class="playerOne"></div>' || casa[7].innerHTML === '<div class="playerTwo"></div>')
     {
         jogada = false;
     }
@@ -204,11 +255,13 @@ casa[7].addEventListener ('click', function () {
 
         casa[7].appendChild(playerAtual);
     }
+
+    verificarVitoria();
 });
 
 casa[8].addEventListener ('click', function () {
 
-    if(casa[8].innerHTML === '<div class="playerOne"></div>' || casa[1].innerHTML === '<div class="playerTwo"></div>')
+    if(casa[8].innerHTML === '<div class="playerOne"></div>' || casa[8].innerHTML === '<div class="playerTwo"></div>')
     {
         jogada = false;
     }
@@ -229,5 +282,6 @@ casa[8].addEventListener ('click', function () {
 
         casa[8].appendChild(playerAtual);
     }
-});
 
+    verificarVitoria();
+});
